aae3b99fa7edf8bd61adda4cf675f7cbe924a96a
Andrew Palmer <andyp.123@gmail.com>
2025-03-20 12:39:57 +0900

    Clean up game of life code, which is all in a single function now

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -1,5 +1,6 @@
 # I2C Setup for my particular screen (ssd1306) taken from Random Nerd Tutorials
 # Complete project details at https://RandomNerdTutorials.com/raspberry-pi-pico-ssd1306-oled-micropython/
+# Uses Adafruit ssd1306 driver
 
 from machine import Pin, SoftI2C
 import ssd1306
@@ -9,7 +10,7 @@ import random
 target_framerate = 10
 ticks_per_second = int(1000 / target_framerate)
 
-#You can choose any other combination of I2C pins
+# You can choose any other combination of I2C pins
 i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
 oled_width = 128
@@ -24,8 +25,7 @@ oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 # 3. Any cell with more than 3 neighbours dies
 # 4. Any dead cell with exactly 3 neighbours becomes live
 
-# Apparently you can do x, y = y, x in Python to swap variables
-cell_size = 2
+cell_size = 2 # width and height of cells when drawn on the display. 1 is very slow!
 cells_w = int(oled_width / cell_size)
 cells_h = int(oled_height / cell_size)
 prev_states = [0] * (cells_w * cells_h)
@@ -48,30 +48,7 @@ def fill_random_states(states):
 fill_random_states(curr_states)
 
 
-def get_live_neighbour_count(i, x, y, states, w, h):
-    # Calculate offsets (will wrap neighbours)
-    size = w * h
-    lofs = -1 if x > 0 else w-1
-    rofs = 1 if x < w-1 else -w+1
-    tofs = -w if y > 0 else size-w
-    bofs = w if y < h-1 else -size+w
-
-    # Calculate neighbours
-    count = 0
-    count += states[i + tofs + lofs] # 1: Top Left
-    count += states[i + tofs]        # 2: Top
-    count += states[i + tofs + rofs] # 3: Top Right
-    count += states[i + lofs]        # 4: Left
-    count += states[i + rofs]        # 5: Right
-    count += states[i + bofs + lofs] # 6: Bottom Left
-    count += states[i + bofs]        # 7: Bottom
-    count += states[i + bofs + rofs] # 8: Bottom Right
-    
-    # print("i,x,y: {},{},{}, lofs: {}, rofs: {}, tofs: {}, bofs: {} | COUNT={}".format(i, x, y, lofs, rofs, tofs, bofs, count))
-    
-    return count
-
-
+# Everything merged into one function gives the best performance
 def update():
     global cells_w, cells_h, cell_size
     global prev_states, curr_states
@@ -103,10 +80,8 @@ def update():
         for x in range(w):
             i = y * w + x
             
-            # count = get_live_neighbour_count(i, x, y, prev_states, cells_w, cells_h)
-            # Putting this code inline seems to give a minor performance uplift
-            # A quick check shows it takes about 150ms for the inlined version and 230 for the function call version
-
+            # Putting this code inline seems to give a minor performance uplift vs a separate function
+            # Currently it takes about 150ms for the inlined version and 230ms for the function call version
             lofs = -1 if x > 0 else w-1
             rofs = 1 if x < w-1 else -w+1
             tofs = -w if y > 0 else size-w
@@ -131,6 +106,7 @@ def update():
             curr_states[i] = state
             curr_living += state
             
+            # Draw the current cell if it is alive
             if state == 1:
                 for cy in range(cell_size):
                     for cx in range(cell_size):
@@ -145,40 +121,19 @@ def update():
         prev_living = curr_living
 
 
-def draw():
-    global cells_w, cells_h, cell_size
-    global curr_states
-    
-    oled.fill(0)
-    
-    for y in range(cells_h):
-        for x in range(cells_w):
-            i = y * cells_w + x
-            
-            # Draw cell
-            if curr_states[i] == 1:
-                for cy in range(cell_size):
-                    for cx in range(cell_size):
-                        oled.pixel(x * cell_size + cx, y * cell_size + cy, 1)
-            #oled.pixel(x, y, curr_states[i])
-            #print("{} ({},{}): {}".format(i,x,y,curr_states[i]))
-    
-    oled.show()
-
-
 def refresh():
     update()
-#     draw()
 
 
-# main loop
+# Main loop
 while True:
     ticks = time.ticks_ms()
     
     refresh()
     
     # Currently I'm not even hitting 10fps, so perhaps this is not very useful!
+    # This time keeping code sucks anyway...
     tick_delta = time.ticks_diff(time.ticks_ms(), ticks)
-    time.sleep_ms(ticks_per_second - tick_delta)
-    print(tick_delta)
+    #time.sleep_ms(ticks_per_second - tick_delta)
+    #print(tick_delta)
 
